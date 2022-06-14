c862ea7a33beb4d077207b44e45cf2a15ebe9d2d
KIMMIJIDLQSLEK <meeji222@naver.com>
2022-06-15 00:37:29 +0900

    Article모델 생성&admin 등록

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from user.models import User
 
 # Create your models here.
 class Category(models.Model):
@@ -10,3 +11,13 @@ class Category(models.Model):
 
     class Meta:
         verbose_name_plural='Categories'
+
+class Article(models.Model):
+    author=models.ForeignKey(to=User,verbose_name="글작성자",on_delete=models.CASCADE) #user와 일대다관계
+    title=models.CharField("글제목",max_length=50)
+    category=models.ManyToManyField(to=Category,verbose_name="카테고리") #category와 다대다관계
+    contents=models.TextField("글내용")
+    created_at=models.DateField("작성날짜",auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.author}의 게시물: {self.title}"
