66ecadb610d7923ddf744637add265729bbe4a5d
KIMMIJIDLQSLEK <meeji222@naver.com>
2022-06-15 00:03:55 +0900

    Category 모델 생성&admin에 등록

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name=models.CharField("카테고리 이름",primary_key=True, max_length=100)
+    introduction=models.TextField("설명")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural='Categories'
