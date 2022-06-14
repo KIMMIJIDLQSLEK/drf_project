b98276c2e5fe37771bdbce34243885dbc56b1852
KIMMIJIDLQSLEK <meeji222@naver.com>
2022-06-15 00:02:59 +0900

    User의 admin형식 바꾸기

--- user/admin.py
+++ user/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group
-from .models import User
+from .models import User,UserProfile
 
 #Group빼기
 admin.site.unregister(Group)
 
+class UserAdmin(admin.ModelAdmin):
+    list_display=['id','username']
+
 #user모델 등록
-admin.site.register(User)
+admin.site.register(User,UserAdmin)
+admin.site.register(UserProfile)
