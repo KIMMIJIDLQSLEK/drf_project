7245ef5f6e6d2897a381e1206c4a0fc1ee8161f3
KIMMIJIDLQSLEK <meeji222@naver.com>
2022-06-20 18:07:52 +0900

    Feat: blog 생성 기능 추가

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework import permissions
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Article,Category
 
 # Create your views here.
+class CreateBlog(APIView):
+    permissions=[permissions.IsAuthenticated]
+    def post(self,request):
+        user=request.user
+        title=request.data.get('title')
+        categories=request.data.get('category')
+        contents=request.data.get('content')
+        Article.objects.create(
+            author=user,
+            title=title,
+            contents=contents,
+        )
+        # for category in categories:
+        #     category_name=Category.objects.get(name=category)
+
+
+
+        return Response({"message":"게시글 생성 완료"},status=status.HTTP_200_OK)
+
