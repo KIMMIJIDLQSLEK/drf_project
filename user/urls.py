0c5296e24b0b06e0a3f318271a88971b5531b951
KIMMIJIDLQSLEK <meeji222@naver.com>
2022-06-20 11:50:36 +0900

    로그인기능 추가

---
+++ user/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import UserView
+
+
+urlpatterns=[
+    path('login/',UserView.as_view()),
+]
--- user/views.py
+++ user/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework import permissions
+from rest_framework.response import Response
+from django.contrib.auth import authenticate,login
+from rest_framework import status
 
 # Create your views here.
+class UserView(APIView):
+    permission_classes = [permissions.AllowAny]
+
+    def get(self,request):
+        return Response({"message":"get"})
+
+    def post(self,request):
+        username=request.data.get('username','')
+        password=request.data.get('password','')
+        email=request.data.get('email','')
+
+        user=authenticate(request,username=username,password=password,email=email)
+        print(user)
+        if not user:
+            return Response({"message":"존재하지 않는 계정이거나 아이디와 비밀번호가 일치하지 않습니다."},status=status.HTTP_401_UNAUTHORIZED)
+
+        login(user)
+        return Response({"message":"로그인완료"},status=status.HTTP_200_OK)
