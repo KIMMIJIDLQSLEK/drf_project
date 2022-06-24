bd287f25b3e3629fc2ac3dcbef15c20a0daa126b
KIMMIJIDLQSLEK <meeji222@naver.com>
2022-06-24 13:43:28 +0900

    Feat: Event 모델 생성

--- product/models.py
+++ product/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Event(models.Model):
+    title=models.CharField("이벤트 제목",max_length=100)
+    preview=models.CharField("미리보기",max_length=100)
+    introduction=models.CharField("설명",max_length=200)
+    created_at=models.DateField("등록일자",auto_now_add=True)
+    started_at=models.DateField("노출시작일",auto_now=True)
+    ended_at=models.DateField("노출종료일",auto_now=True)
+    activate=models.BooleanField("활성화여부")
+
