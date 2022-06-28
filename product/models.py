7cd63b44472c9c720b2c9c7a3c7aa54a3abed175
KIMMIJIDLQSLEK <meeji222@naver.com>
2022-06-28 16:35:17 +0900

    Refactor: Event모델 변경

--- product/models.py
+++ product/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Event(models.Model):
     title=models.CharField("이벤트 제목",max_length=100)
-    preview=models.FileField("썸네일",upload_to='product/files/%Y/%m/%d/',blank=True)
+    thumbnail=models.ImageField("썸네일",upload_to='images/product/',blank=True)
     introduction=models.CharField("설명",max_length=200)
     created_at=models.DateField("등록일자",auto_now_add=True)
     started_at=models.DateTimeField("노출시작일",)
