32d1d68f32585e7fbce5deae98a5dc190804a16b
KIMMIJIDLQSLEK <meeji222@naver.com>
2022-06-28 16:38:28 +0900

    Refactor: UserAdmin사용하여 비밀번호 해쉬하도록 custom admin 수정

--- user/admin.py
+++ user/admin.py
@@ -1,12 +1,26 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group
 from .models import User,UserProfile
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 #Group빼기
 admin.site.unregister(Group)
 
-class UserAdmin(admin.ModelAdmin):
-    list_display=['id','username']
+class UserAdmin(BaseUserAdmin):
+    #UserAdmin을 사용할시 필수!
+    list_display = ("id","username", "password","email", "nickname", "is_active")
+    list_filter = ("username",)
+    filter_horizontal = []
+
+    #선택
+    list_display_links = ('username',)
+    readonly_fields = ('created_at',)
+    fieldsets = (
+        ('info',{'fields':('username','password','email','nickname','created_at')}),
+        ('permissions',{'fields':('is_active','is_admin')})
+    )
+
+
 
 #user모델 등록
 admin.site.register(User,UserAdmin)
