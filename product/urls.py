5869ae084010232d15c13172f5d6f9f120344878
KIMMIJIDLQSLEK <meeji222@naver.com>
2022-06-28 16:37:01 +0900

    Feat: product admin에서 저장한 사진 보여주기 기능추가

--- product/urls.py
+++ product/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import EventView
+from .views import EventView,EventThumbnailView
 
 urlpatterns=[
     path('event/',EventView.as_view()),
+    #이메일 url
+    path('thumbnail/<obj_id>/',EventThumbnailView.as_view()),
 ]
--- product/views.py
+++ product/views.py
@@ -1,9 +1,13 @@
+import os.path
+
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework import permissions
 from .serializers import EventSerializer
 from rest_framework.response import Response
 from rest_framework import status
+from .models import Event
+from django.views.static import serve
 
 # Create your views here.
 class EventView(APIView):
@@ -20,5 +24,18 @@ class EventView(APIView):
         event_serializer.save()
 
         return Response(event_serializer.data,status=status.HTTP_200_OK)
+
+class EventThumbnailView(APIView):
+    permission_classes = []
+    #TODO
+    #get으로 EventThumbnail 링크 가져오기
+    def get(self,request,obj_id):
+        #1.Event모델 가져오기
+        #2.Event모델의 썸네일 경로 가져오기
+        #3.경로 보내기
+        event=Event.objects.get(id=obj_id)
+        thumbnail_path=event.thumbnail.path
+        return serve(request,os.path.basename(thumbnail_path),os.path.dirname(thumbnail_path))
+
 
 
