e243f190b153388fcc8feb9d3be7e47a74966389
KIMMIJIDLQSLEK <meeji222@naver.com>
2022-06-28 16:37:01 +0900

    Feat: product admin에서 저장한 사진 보여주기 기능추가

--- product/admin.py
+++ product/admin.py
@@ -1,5 +1,22 @@
 from django.contrib import admin
 from .models import Event
+from django.utils.safestring import mark_safe
+
+class EventAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'thumbnail',
+        'introduction',
+        'started_at',
+        'ended_at',
+        'activate',
+        'thumbnail_preview'
+    )
+
+    def thumbnail_preview(self,obj):
+        return mark_safe(f'<img src="/product/thumbnail/{obj.id}/" height="150px"/>')
+
+
 
 # Register your models here.
-admin.site.register(Event)
+admin.site.register(Event,EventAdmin)
