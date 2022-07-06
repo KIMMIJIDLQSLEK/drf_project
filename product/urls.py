fedb17dc249edddb23a63a9a244a78156e9e2f14
KIMMIJIDLQSLEK <meeji222@naver.com>
2022-07-06 11:10:02 +0900

    추가 Products 전체 조회

--- product/urls.py
+++ product/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import ProductAllView,ProductImgView
+from .views import ProductView,ProductImgView
 
 urlpatterns=[
-    path('',ProductAllView.as_view()),
+    path('',ProductView.as_view()),
     #이메일 url
     path('img/<obj_id>/',ProductImgView.as_view()),
 ]
